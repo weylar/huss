1a5884191262f4857e2e50e7870acb433702e7c4
hadeoh <usthmandanfodio@gmail.com>
2020-01-24 17:15:52 -0800

    Edit simaliar ads response

--- api/server/services/category.js
+++ api/server/services/category.js
@@ -106,7 +106,7 @@ class CategoryService {
 
   static async getPopularCategories() {
 
-    const popularCategories = await db.Category.findAll({limit: 5, order: [['belongedAd', 'ASC']]});
+    const popularCategories = await db.Category.findAll({limit: 5, order: [['belongedAd', 'DESC']]});
 
     return {
       status: 'success',
